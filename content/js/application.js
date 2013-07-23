ee629209fe57085f978fdd6405445097eaf8d6b2
Muzyka Lukasz <muzyka.lukasz@gmail.com>
2013-07-23 13:14:03 +0800

    major refactoring

--- content/js/application.js
+++ content/js/application.js
@@ -1,15 +1,17 @@
 $(function(){
-  $('#home .navigation').addClass('hidden');
+    $('.header .navigation').addClass('hidden');
     $('.menu-link').on('click', function(e){
         e.preventDefault();
-        $('.top-bar nav').toggleClass('hidden');
+        $('.header nav').toggleClass('hidden');
     });
 
     $(window).scroll(function(){
         if($(window).scrollTop() <= 20){
-            $('#home').removeClass('alt')
+            $('.header').removeClass('alt')
         } else {
-            $('#home').addClass('alt')
+            $('.header').addClass('alt')
         }
     });
 });
+
+
